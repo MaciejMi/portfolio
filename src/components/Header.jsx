5c09163435b61e38d5ef66cc9e39e4d97a016373
Maciej <maciejmich22@gmail.com>
2025-07-29 17:29:37 +0200

    add header content and fix background

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,6 +12,16 @@ const Header = () => {
 					alt="Maciej w niebieskim garniturze oparty o naczepę ciężarówki, z założonymi rękami i wyprostowaną postawą, patrzący w dal "
 					isOnline={true}
 				/>
+				<div className="header__content">
+					<h1>Maciej Michalczyk</h1>
+					<h2>Full-Stack Developer</h2>
+					<p>
+						<i class="ti ti-map-pin"></i> Kielce, Polska
+					</p>
+					<p>
+						Tworzę <span>aplikacje internetowe</span> w <span>React.js</span> i <span>Express.js</span>.
+					</p>
+				</div>
 			</div>
 		</header>
 	)
