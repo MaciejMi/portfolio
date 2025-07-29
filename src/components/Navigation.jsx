4c8d2d65953eca69b8a945966e08a7ebee19616a
Maciej <maciejmich22@gmail.com>
2025-07-29 16:09:44 +0200

    add functionality to navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,14 +1,25 @@
+import { useState } from 'react'
 import logo from '../assets/imgs/logo.png'
 import '../assets/navigation.scss'
 
 const Navigation = () => {
+	const [isShowMenu, setIsShowMenu] = useState(false)
+	const onMenuButtonHandle = () => {
+		setIsShowMenu(previousState => !previousState)
+	}
+
 	return (
 		<nav className="nav">
 			<div className="wrapper">
-				<a href="#" className="nav__logo">
-					<img src={logo} alt="logo" />
-				</a>
-				<div className="nav__links">
+				<div className="nav__top">
+					<a href="#" className="nav__logo">
+						<img src={logo} alt="logo" />
+					</a>
+					<button className="nav__menu" onClick={onMenuButtonHandle}>
+						<i className={isShowMenu ? 'ti ti-x' : 'ti ti-menu-2'}></i>
+					</button>
+				</div>
+				<div className={isShowMenu ? 'nav__links' : 'nav__links nav__hide'}>
 					<a href="#" className="nav__item">
 						O mnie
 					</a>
